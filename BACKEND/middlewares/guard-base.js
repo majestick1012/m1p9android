4da9cd1d8d1b49c240a67aadce41e1177ba0baab
Mirado RANDRIAMBOLOLONA <miradorandriambololona@gmail.com>
2022-05-09 17:13:38 +0300

    Middlewares

---
+++ BACKEND/middlewares/guard-base.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_BASE);
+    
+    req.userData = {
+      email: decodedToken.email
+    };
+    next();
+  } catch (error) {
+    res.status(403).json({ message: "Forbidden!" });
+  }
+}
