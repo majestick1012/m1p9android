bdaad7820727e1307c277bc8b964401af856bc0a
JohanGitHubMaster <rakotovaojohan516@gmail.com>
2022-05-10 08:47:57 +0300

    ajout de model view controller

--- ANDROID/app/src/main/java/com/example/jeueducatif/view/MainActivity.java
+++ ANDROID/app/src/main/java/com/example/jeueducatif/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.jeueducatif;
+package com.example.jeueducatif.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.jeueducatif.R;
 
 public class MainActivity extends AppCompatActivity {
 
