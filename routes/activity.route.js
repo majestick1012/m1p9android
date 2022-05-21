eec1706b60c2e917102ef639c495efacfaa54e07
MajeStick <rhmirado@gmail.com>
2022-05-21 23:14:49 +0300

    catch ValidationError

--- routes/activity.route.js
+++ routes/activity.route.js
@@ -22,26 +22,51 @@ router.get("/all", (req, res, next) => {
         });
       }
     })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).json({
+          success: false,
+          message: err.message,
+        });
+      }
+      return res.status(500).json({
+        success: false,
+        message: err,
+      });
+    });
 });
 
 // GET BY ID
 router.get("/:id", (req, res, next) => {
   console.log(req.params.id);
-  Activity.findById(req.params.id).then((result) => {
-    if (result) {
-      return res.status(200).json({
-        success: true,
-        message: "Getting the activity successfully!",
-        data: result
+  Activity.findById(req.params.id)
+    .then((result) => {
+      if (result) {
+        return res.status(200).json({
+          success: true,
+          message: "Getting the activity successfully!",
+          data: result,
+        });
+      } else {
+        return res.status(204).json({
+          success: true,
+          message: "Aucun resultat",
+          data: {},
+        });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).json({
+          success: false,
+          message: err.message,
+        });
+      }
+      return res.status(500).json({
+        success: false,
+        message: err,
       });
-    } else {
-      return res.status(204).json({
-        success: true,
-        message: "Aucun resultat",
-        data: {},
-      });
-    }
-  });
+    });
 });
 
 module.exports = router;
--- routes/parameter.route.js
+++ routes/parameter.route.js
@@ -5,21 +5,34 @@ const router = express.Router();
 
 // GET ALL
 router.get("/all", (req, res, next) => {
-  Parameter.find({}).then((result) => {
-    if (!result) {
-      return res.status(204).json({
-        success: true,
-        message: "Aucun resultat",
-        data: [],
+  Parameter.find({})
+    .then((result) => {
+      if (!result) {
+        return res.status(204).json({
+          success: true,
+          message: "Aucun resultat",
+          data: [],
+        });
+      } else {
+        return res.status(200).json({
+          success: true,
+          message: "Parameters fetched successfully!",
+          data: result,
+        });
+      }
+    })
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        return res.status(400).json({
+          success: false,
+          message: err.message,
+        });
+      }
+      return res.status(500).json({
+        success: false,
+        message: err,
       });
-    } else {
-      return res.status(200).json({
-        success: true,
-        message: "Parameters fetched successfully!",
-        data: result,
-      });
-    }
-  });
+    });
 });
 
 module.exports = router;
